9b2d8734ec42ec5074a81e89f90ab7d6081a51ff
AVRozk <avrozk@gmail.com>
2021-03-11 03:01:01 +0300

    Remove main view controller

--- UberClone/Controller/LoginController.swift
+++ UberClone/Controller/LoginController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginController.swift
 //  UberClone
 //
 //  Created by andy on 11.03.2021.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.backgroundColor = .red
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
